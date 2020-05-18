f3a30c969cf80bf0515277469666d0b7457d1a2b
Nick Paz <nicholasmpaz@gmail.com>
2020-05-18 11:56:10 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,52 @@ do
 done
 echo "Deploying to $environment.";
 
-# install global dependencies
+# install and initialize amplify
 
 sudo npm install -g @aws-amplify/cli
+
+cd frontend
+
+export VUECONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"dist\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script serve\"\
+}"
+
+export AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"ap-southeast-1\"\
+}"
+
+export FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"vue\",\
+\"config\":$VUECONFIG\
+}"
+
+export AMPLIFY="{\
+\"projectName\":\"myproject\",\
+\"envName\":\"prod\",\
+\"defaultEditor\":\"vscode\"\
+}"
+
+export PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify init \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
+
+cd ..
+
 # sudo npm install -g serverless
 
 # deploy frontend
