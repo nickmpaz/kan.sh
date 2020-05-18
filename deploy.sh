7af4579d07de2d19cdd34cc595717af18f269f4f
Nick Paz <nicholasmpaz@gmail.com>
2020-05-18 15:32:55 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -14,88 +14,88 @@ esac
 
 echo "Deploying to $environment.";
 
-# install and initialize amplify
+# # install and initialize amplify
 
-sudo npm install -g @aws-amplify/cli
+# sudo npm install -g @aws-amplify/cli
 
-mkdir ~/.aws && touch ~/.aws/credentials && touch ~/.aws/config
+# mkdir ~/.aws && touch ~/.aws/credentials && touch ~/.aws/config
 
-cd frontend
+# cd frontend
 
-export VUECONFIG="{\
-\"SourceDir\":\"src\",\
-\"DistributionDir\":\"dist\",\
-\"BuildCommand\":\"npm run-script build\",\
-\"StartCommand\":\"npm run-script serve\"\
-}"
+# export VUECONFIG="{\
+# \"SourceDir\":\"src\",\
+# \"DistributionDir\":\"dist\",\
+# \"BuildCommand\":\"npm run-script build\",\
+# \"StartCommand\":\"npm run-script serve\"\
+# }"
 
-export AWSCLOUDFORMATIONCONFIG="{\
-\"configLevel\":\"project\",\
-\"useProfile\":false,\
-\"profileName\":\"default\",\
-\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
-\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
-\"region\":\"us-east-1\"\
-}"
+# export AWSCLOUDFORMATIONCONFIG="{\
+# \"configLevel\":\"project\",\
+# \"useProfile\":false,\
+# \"profileName\":\"default\",\
+# \"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+# \"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+# \"region\":\"us-east-1\"\
+# }"
 
-export FRONTEND="{\
-\"frontend\":\"javascript\",\
-\"framework\":\"vue\",\
-\"config\":$VUECONFIG\
-}"
+# export FRONTEND="{\
+# \"frontend\":\"javascript\",\
+# \"framework\":\"vue\",\
+# \"config\":$VUECONFIG\
+# }"
 
-export AMPLIFY="{\
-\"projectName\":\"myproject\",\
-\"envName\":\"prod\",\
-\"defaultEditor\":\"vscode\"\
-}"
+# export AMPLIFY="{\
+# \"projectName\":\"myproject\",\
+# \"envName\":\"prod\",\
+# \"defaultEditor\":\"vscode\"\
+# }"
 
-export PROVIDERS="{\
-\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
-}"
+# export PROVIDERS="{\
+# \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+# }"
 
-amplify init \
---amplify $AMPLIFY \
---frontend $FRONTEND \
---providers $PROVIDERS \
---yes
+# amplify init \
+# --amplify $AMPLIFY \
+# --frontend $FRONTEND \
+# --providers $PROVIDERS \
+# --yes
 
-cd ..
+# cd ..
 
-# initialize terraform 
+# # initialize terraform 
 
-cd terraform 
+# cd terraform 
 
-terraform init
+# terraform init
 
-cd ..
+# cd ..
 
-# install serverless and plugins 
+# # install serverless and plugins 
 
-cd backend
+# cd backend
 
-npm ci
+# npm ci
 
-./node_modules/.bin/serverless plugin install --name serverless-python-requirements
+# ./node_modules/.bin/serverless plugin install --name serverless-python-requirements
 
-cd ..
+# cd ..
 
-# deploy frontend
+# # deploy frontend
 
-cd frontend
+# cd frontend
 
-npm ci
+# npm ci
 
-npm run build:$environment
+# npm run build:$environment
 
-aws s3 sync --delete ./dist s3://$(cd ../terraform && terraform output bucket_$environment)
+# aws s3 sync --delete ./dist s3://$(cd ../terraform && terraform output bucket_$environment)
 
-cd ..
+# cd ..
 
-# deploy backend
+# # deploy backend
 
-cd backend
+# cd backend
 
-./node_modules/.bin/serverless deploy --stage $environment
+# ./node_modules/.bin/serverless deploy --stage $environment
 
-cd ..
+# cd ..
